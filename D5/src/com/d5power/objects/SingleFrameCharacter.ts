ee41efe4a990d30818e1bbd172b1c864545b1b21
D5Power <D5PowerStudio@gmail.com>
2020-06-17 17:59:05 +0800

    update
    Fixed bug in SingleFrameCharacter

--- D5/src/com/d5power/objects/SingleFrameCharacter.ts
+++ D5/src/com/d5power/objects/SingleFrameCharacter.ts
@@ -2,7 +2,9 @@ module d5power
 {
     export class SingleFrameCharacter extends GameObject implements IGD
     {
-        
+        private _skinReady:boolean = false;
+        private _offX:number;
+        private _offY:number;
         public constructor(map:IMap)
         {
             super(map);
@@ -12,25 +14,35 @@ module d5power
 
         public setSkin(path:string):void
         {
-            var data:D5UIResourceData = D5UIResourceData.getData(name);
+            var data:D5UIResourceData = D5UIResourceData.getData(path);
             if(data==null)
             {
-                trace("[D5Bitmap]No Resource"+name);
-                var texture:egret.Texture = RES.getRes(name);
+                trace("[D5Bitmap]No Resource"+path);
+                var texture:egret.Texture = RES.getRes(path);
                 if(texture)
                 {
-                    (<egret.Bitmap>this._monitor).texture = texture;
+                    this.onResReady(texture);
                 }else
                 {
-                    RES.getResByUrl(name,this.onResReady,this,RES.ResourceItem.TYPE_IMAGE);
+                    RES.getResByUrl(path,this.onResReady,this,RES.ResourceItem.TYPE_IMAGE);
                 }
                 return;
             }
             (<egret.Bitmap>this._monitor).texture = data.getResource(0);
         }
 
+        private _lastTimer:number;
+        public run(t:number):void
+        {
+            if(!this._skinReady) return;
+            this.updatePos(this._offX,this._offY);
+        }
+
         private onResReady(data:egret.Texture):void
         {
+            this._skinReady = true;
+            this._offX = -data.textureWidth>>1;
+            this._offY = -data.textureHeight;
             (<egret.Bitmap>this._monitor).texture = data;
         }
     }
